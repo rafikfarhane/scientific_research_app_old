83e18786190e0ec21f5298391efb2d04da895d61
Lukas Baumeister <s05lbaum@uni-bonn.de>
2024-05-13 20:51:12 +0200

    Python kann jetzt die Daten für die Projects liefern (name, ...)

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/dashboard")
 def home():
-    return render_template('test.html')
+    first = ["Name1", "Open", "Writer", "Lücke + 9 Members", "24.05.2024"]
+    second = ["Name2", "Open", "Writer", "Neuer + 3 Members", "27.05.2024"]
+    third = ["Name4", "Closed", "Writer", "Vini + 5 Members", "29.05.2024"]
+    projects = [first,second,third]
+    return render_template('test.html', textForColumn=projects)
 
 
 if __name__ == "__main__":
