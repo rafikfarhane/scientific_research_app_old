0c8380bd454da4a4fcd4a54ed5533925aae27375
Torge Rau <s37trau@uni-bonn.de>
2024-05-13 16:24:59 +0200

    NewProject UI gestartet

--- NewProject.py
+++ NewProject.py
@@ -1,2 +1,11 @@
-def UI():
-    None
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/NewProject")
+def NewProject():
+    return render_template("NewProjectUI.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
