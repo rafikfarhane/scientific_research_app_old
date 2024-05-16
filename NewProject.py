80ba390edfc48ba6c94fda011c634dde8fd14648
Torge Rau <s37trau@uni-bonn.de>
2024-05-16 15:21:50 +0200

    Alle Aenderungen aus der Pull request umgesetzt

--- NewProject.py
+++ NewProject.py
@@ -9,7 +9,7 @@ project_nutzer = []
 project_funding = []
 
 @app.route("/NewProject")
-def NewProject():
+def new_project():
     #rendering the NewProject page with project_nutzer and project_funding as parameters    
     return render_template("NewProjectUI.html", nutzer=project_nutzer, funder=project_funding)
 
@@ -17,20 +17,20 @@ def NewProject():
 @app.route("/NewProject/nutzer_hinzufuegen", methods=['POST'])
 def nutzer_hinzufuegen():
     #request from the html where forms have the name name. Because this is an input the typed name of the new member is selected.
-    name = request.form['name']
+    nutzer = request.form['name']
     #the new Member is appended to the project_nutzer list
-    project_nutzer.append(name)
+    project_nutzer.append(nutzer)
     #with redirect(url_for) we directly get back to the NewProject page
-    return redirect(url_for("NewProject"))
+    return redirect(url_for("new_project"))
 
 @app.route("/NewProject/funding_hinzufuegen", methods=['POST'])
 def funding_hinzufuegen():
     #request from the html where forms have the name name. Because this is an input the typed name of the new funder is selected
-    name = request.form['name']
+    funder = request.form['name']
     #the new funder is added to the project_funder list
-    project_funding.append(name)
+    project_funding.append(funder)
     #redirect to the NewProject page
-    return redirect(url_for("NewProject"))
+    return redirect(url_for("new_project"))
 
 
 if __name__ == "__main__":
