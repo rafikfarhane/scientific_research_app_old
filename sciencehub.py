5c3a561c23c87a6b089a4ddec16495ac1bfd30f8
Imad <s47iaziz@uni-bonn.de>
2024-05-28 15:59:36 +0200

    Pfad erstellt sodass beim erstellen eines Projekts die Daten gespeichert werden

--- sciencehub.py
+++ sciencehub.py
@@ -79,68 +79,148 @@ def funding_hinzufuegen():
 
 
 
-class Database:
+@app.route("/NewProject/create_project")
+def create_project():
+    # platzhalter
+    nid = "abs12345"
 
-    def __init__(self):
-        self.id = None
+    # Objekt der Datenbank erzeugen und connecten
+    db = Database(nid)
+
+    conn = db.p_create_connection()
+
+    project_table = """CREATE TABLE IF NOT EXISTS PROJECT (
+                      PID VARCHAR(40) PRIMARY KEY,
+                      NAME VARCHAR(30) NOT NULL,
+                      DESCRIPTION TEXT NOT NULL,
+                      ADMIN VARCHAR(40) NOT NULL,
+                      FUNDER TEXT
+                ); """
+    
+    user_table = f"""
+                CREATE TABLE IF NOT EXISTS {nid} (
+                      PID VARCHAR(40) NOT NULL,
+                      ROLE VARCHAR(5) NOT NULL,
+                      FOREIGN KEY (PID) REFERENCES PROJECT(PID)
+                );
+            """
+    
+    # User Tabelle erstellen
+    db.create_table(conn, user_table)
+
+    # PID erstellen
+    pid = str(uuid.uuid4())
+    # Die NID des Admins vom Projekt                         
+    user_admin = nid
+    # Tupel erstellen                   
+    u_values = (pid, 'admin')      
+    # Tupel in Tabelle einfügen                     
+    db.p_insert_user(conn, u_values)  
+
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM \"{nid}\"")
+
+    # Spaltennamen abrufen
+    column_names = [description[0] for description in cursor.description]
+    print(" | ".join(column_names))
+
+    # Zeilen abrufen und drucken
+    rows = cursor.fetchall()
+    for row in rows:
+        print(" | ".join(map(str, row)))
+
+
+    project_name = new_project_info['project_name']
+    project_description = new_project_info['project_description']
+    project_funder = new_project_info['project_funder']
+    project_funder_str = ','.join(project_funder)
+    
+    # Projekt Tabelle erstellen
+    db.create_table(conn, project_table)       
+    # Tupel erstellen
+    
+    p_values = (pid, project_name, project_description, 
+                user_admin, project_funder_str)
+    # Tupel in Projekt Tabelle einfügen
+    
+    db.p_insert_project(conn, p_values)   
+
+                        
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM PROJECT")
+
+    # Spaltennamen abrufen
+    column_names = [description[0] for description in cursor.description]
+    print(" | ".join(column_names))
+
+    # Zeilen abrufen und drucken
+    rows = cursor.fetchall()
+    for row in rows:
+        print(" | ".join(map(str, row)))
+
+    
+    new_project_info["project_name"] = ""
+    new_project_info["project_description"] = ""
+    new_project_info["project_funder"] = []
+    new_project_info["project_member"] = []
+
+
+    """
+    # Plan: Die PID zu den einzelnen Member Tabellen einfügen, aber wie?
+    for member in request.form.getlist('project_members'):          # Besprechen!!!
+        db.p_insert_user(conn, member, pid, 'read')
+    """
+
+
+
+    return redirect(url_for("dashboard"))
+
+
+
+
+
+
+class Database:
+    # Konstruktor mit der ID des Nutzers
+    def __init__(self, nid):
+        self.id = nid
         self.p_db_name = 'project.db'
         self.l_db_name = "nutzerdaten.db"
 
 
-
+    # Funktion zur Verbindung mit der Datenbank herstellen
     def p_create_connection(self):
         """Erstellen einer Datenbankverbindung zu einer SQLite-Datenbank"""
         p_conn = None
         try:
             p_conn = sqlite3.connect(self.p_db_name)
             return p_conn
-        except sqlite3.Error as e:
+        # Diese Zeilen fangen Fehler ab
+        except sqlite3.Error as e:                   
             print(e)
         return p_conn
 
 
-
-    def p_user_table(self, p_conn):
-        # Erstelle Tabelle für den User
+    # Datenbank Tabelle für Projekte erstellen
+    def create_table(self, p_conn, sqlcode):
         try:
             c = p_conn.cursor()
-            c.execute(f"""
-                CREATE TABLE IF NOT EXISTS {self.id} (
-                      PID VARCHAR(40) NOT NULL,
-                      ROLE VARCHAR(5) NOT NULL,
-                      FOREIGN KEY (PID) REFERENCES PROJECT(PID)
-                );
-            """)
+            c.execute(sqlcode)
             p_conn.commit()
         except sqlite3.Error as e:
             print(e)
 
 
-
-    def p_project_tabe(self, p_conn):
-        # Erstelle Tabelle für Projekte
-        try:
-            c = p_conn.cursor()
-            c.execute(f"""
-                CREATE TABLE IF NOT EXISTS PROJECT (
-                      PID VARCHAR(40) PRIMARY KEY,
-                      NAME VARCHAR(30) NOT NULL,
-                      DESCRIPTION TEXT NOT NULL,
-                      ADMIN VARCHAR(40) NOT NULL,
-                      FUNDER TEXT
-                );
-            """)
-            p_conn.commit()
-        except sqlite3.Error as e:
-            print(e)
-
-
-
+    # Tupel in die Nutzer Tabelle einfügen
     def p_insert_user(self, p_conn, tupel):
         """Values werden in die User Tabelle eingefügt"""
         try:
+            # Values(?,?) sind Platzhalter die vom Tupel ersetzt werden
             sql = f''' INSERT INTO {self.id}(PID, ROLE)
-                    VALUES(?,?) '''
+                    VALUES(?,?) '''                         
             cur = p_conn.cursor()
             cur.execute(sql, tupel)
             p_conn.commit()
@@ -149,7 +229,7 @@ class Database:
             print(e)
 
 
-
+    # Tupel in die Projekt Tabelle einfügen
     def p_insert_project(self, p_conn, p_tupel):
         """Values werden in die Projekt Tabelle eingefügt"""
         try:
@@ -161,7 +241,6 @@ class Database:
             return cur.lastrowid
         except sqlite3.Error as e:
             print(e)
-
 
 
 
