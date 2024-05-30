35056b8fb27c286b99001136fb49bff45103a2ec
Imad <s47iaziz@uni-bonn.de>
2024-05-30 19:12:27 +0200

    Nach Code Review die einzelnen Anforderungen nachgegangen und verbessert

--- sciencehub.py
+++ sciencehub.py
@@ -39,30 +39,30 @@ new_project_info = dict(
 
 @app.route("/NewProject")
 def new_project():
-    # rendering the NewProject page with project_nutzer and project_funding as parameters
+    # rendering the NewProject page with project_user and project_funding as parameters
     return render_template(
         "NewProjectUI.html",
         name_value = new_project_info["project_name"],
         description_value = new_project_info["project_description"],
-        nutzer = new_project_info["project_member"],
+        user = new_project_info["project_member"],
         funder = new_project_info["project_funder"],
     )
 
 
 # function to add a new Member to a newProject
-@app.route("/NewProject/nutzer_hinzufuegen", methods=["POST"])
-def nutzer_hinzufuegen():
+@app.route("/NewProject/add_user", methods=["POST"])
+def add_user():
     # request from the html where forms have the name name. Because this is an input the typed name of the new member is selected.
-    nutzer = request.form["name"]
-    # the new Member is appended to the project_nutzer list
-    new_project_info["project_member"].append(nutzer)
+    user = request.form["name"]
+    # the new Member is appended to the project_user list
+    new_project_info["project_member"].append(user)
     # with redirect(url_for) we directly get back to the NewProject page
     return redirect(url_for("new_project"))
 
 
 # Function to save the projectname and the project description
-@app.route("/NewProject/saveData", methods=["POST"])
-def saveData():
+@app.route("/NewProject/save_data", methods=["POST"])
+def save_data():
     # request the json data which contains the projectname and the projectdescription
     data = request.json
     # set variables to save the project_name and the project_description
@@ -89,8 +89,9 @@ def saveData():
         return jsonify({"message": "Invalid data"}), 400
 
 
-@app.route("/NewProject/funding_hinzufuegen", methods=["POST"])
-def funding_hinzufuegen():
+
+@app.route("/NewProject/add_funding", methods=["POST"])
+def add_funding():
     # request from the html where forms have the name name. Because this is an input the typed name of the new funder is selected
     funder = request.form["name"]
     # the new funder is added to the project_funder list
@@ -100,9 +101,7 @@ def funding_hinzufuegen():
 
 
 
-
-
-# Teste die Funktionalitäten der Database-Klasse visuell
+# Nach dem Entwurf eines Projekts werden die Daten in der Datenbank gespeichert
 @app.route("/NewProject/create_project")
 
 def create_project():
@@ -110,7 +109,7 @@ def create_project():
     # Objekt der Datenbank erzeugen und connecten
     db = Database()
     nid = db.get_id()
-    conn = db.create_connection(db.p_db_name)
+    conn = db.create_connection(db.project_db)
 
     project_table = """CREATE TABLE IF NOT EXISTS PROJECT (
                       PID VARCHAR(40) PRIMARY KEY,
@@ -130,57 +129,45 @@ def create_project():
     
     # User Tabelle erstellen
     db.create_table(conn, user_table)
+
     # PID erstellen
     pid = str(uuid.uuid4())
+
     # Die NID des Admins vom Projekt                         
     user_admin = nid
+
     # Tupel erstellen                   
-    u_values = (pid, 'admin')      
+    user_values = (pid, "admin")      
+
     # Tupel in Tabelle einfügen                     
-    db.p_insert_user(conn, u_values)  
+    db.insert_user(conn, user_values)  
 
-
-    ### TEST: DRUCKE TABELLE ###
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM \"{nid}\"")
-    # Spaltennamen abrufen
-    column_names = [description[0] for description in cursor.description]
-    print(" | ".join(column_names))
-    # Zeilen abrufen und drucken
-    rows = cursor.fetchall()
-    for row in rows:
-        print(" | ".join(map(str, row)))
-
+    # Drucke User Tabelle
+    print_table(conn, nid)
 
     # die benötigten Daten aus dem Dictionary holen
-    project_name = new_project_info['project_name']
-    project_description = new_project_info['project_description']
-    project_funder = new_project_info['project_funder']
-    project_funder_str = ','.join(project_funder)
+    project_name = new_project_info["project_name"]
+    project_description = new_project_info["project_description"]
+    project_funder = new_project_info["project_funder"]
+    project_funder_str = ",".join(project_funder)
     
     # Projekt Tabelle erstellen
-    db.create_table(conn, project_table)       
+    db.create_table(conn, project_table)  
+
     # Tupel erstellen
-    p_values = (pid, project_name, project_description, 
+    project_values = (pid, project_name, project_description, 
                 user_admin, project_funder_str)
+    
     # Tupel in Projekt Tabelle einfügen
-    db.p_insert_project(conn, p_values)   
+    db.insert_project(conn, project_values)   
 
     # Plan: Die PID zu den einzelnen Member Tabellen einfügen
-    for member in new_project_info['project_member']:
-        member_id = db.get_from_name_id(member)
-        db.p_insert_other_user(conn, member_id, (pid, 'read'))
+    for member in new_project_info["project_member"]:
+        member_id = db.get_from_name_id(conn, member)
+        db.add_values_to_member(conn, member_id, (pid, "read"))
 
-
-    ### TEST: DRUCKE TABELLE ###                 
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM PROJECT")
-    column_names = [description[0] for description in cursor.description]
-    print(" | ".join(column_names))
-    rows = cursor.fetchall()
-    for row in rows:
-        print(" | ".join(map(str, row)))
-    
+    # drucke PROJKET Tabelle
+    print_table(conn, "PROJECT")
 
     # Das Dictonary nach dem eingeben wieder leeren
     new_project_info["project_name"] = ""
@@ -192,6 +179,20 @@ def create_project():
 
 
 
+# Druckt die Tabelle
+def print_table(conn, table_name):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM \"{table_name}\"")
+        rows = cursor.fetchall()
+        print(f"Data from {table_name}:")
+        for row in rows:
+            print(row)
+    except sqlite3.Error as e:
+        print(f"Error reading from table {table_name}: {e}")
+
+
+
 ######################################################################################################################################################
 
 
@@ -199,9 +200,9 @@ def create_project():
 class Database:
     def __init__(self):
         self.id = None
-        self.p_db_name = "project.db"
-        self.l_db_name = "sign_in_up_user_data.db"
-        self.l_allUser_db = "all_users.db"
+        self.project_db = "project.db"
+        self.login_db = "sign_in_user_data.db"
+        self.all_users_db = "all_users.db"
 
 
 
@@ -226,7 +227,7 @@ class Database:
 
 
     def generate_user_id(self):
-        """Generiere zufällige user_id"""
+        # Generiere zufällige user_id
         return str(uuid.uuid4())
 
 
@@ -252,18 +253,18 @@ class Database:
 
    
 
-    def l_register_user(self, conn, username, mail, password):
-        # Hinzufügen eines neuen Benutzers zur Tabelle 'l_nutzerdaten
+    def register_user(self, conn, username, mail, password):
+        # Hinzufügen eines neuen Benutzers zur Tabelle "user_data
         password_hash = self.hash_password(password)
         user_id = self.generate_user_id()
         try:
             cursor = conn.cursor()
             cursor.execute(
-                "INSERT INTO l_nutzerdaten (nutzer_id, nutzername, mail, passwort_hash) VALUES (?, ?, ?, ?)",
+                "INSERT INTO user_data (user_id, username, mail, passwort_hash) VALUES (?, ?, ?, ?)",
                 (user_id, username, mail, password_hash),
             )
             conn.commit()
-            self.l_add_user_to_all_users(username,user_id)
+            self.add_user_to_all_users(username,user_id)
             print(f"User added successfully with ID {user_id}.")
         except sqlite3.IntegrityError:
             print("Username or email already exists.")
@@ -272,8 +273,8 @@ class Database:
 
 
 
-    def l_add_user_to_all_users(self,username, user_id):
-        conn = self.create_connection(self.l_allUser_db)
+    def add_user_to_all_users(self,username, user_id):
+        conn = self.create_connection(self.all_users_db)
 
         cursor = conn.cursor()
         cursor.execute(
@@ -283,6 +284,8 @@ class Database:
         conn.commit()
 
         print(f"User {username} was added")
+
+
 
     def get_from_name_id(self, conn, username):
         try:
@@ -306,13 +309,13 @@ class Database:
     
 
 
-    def l_login_user(self, conn, username, password):
+    def login_user(self, conn, username, password):
         # Einloggen eines Benutzers
         password_hash = self.hash_password(password)
         try:
             cursor = conn.cursor()
             cursor.execute(
-                "SELECT nutzer_id, passwort_hash FROM l_nutzerdaten WHERE nutzername = ?",
+                "SELECT user_id, passwort_hash FROM user_data WHERE username = ?",
                 (username,),
             )
             # fetcone -> geht auf die erste Zeile (einzige Zeile)
@@ -338,13 +341,13 @@ class Database:
         db = Database()
 
         # Öffne eine Verbindung zur Datenbank
-        conn = db.create_connection(self.l_db_name)
-        conn_all_users = db.create_connection(self.l_allUser_db)
+        conn = db.create_connection(self.login_db)
+        conn_all_users = db.create_connection(self.all_users_db)
 
         user_table = """
-                CREATE TABLE IF NOT EXISTS l_nutzerdaten (
-                    nutzer_id TEXT PRIMARY KEY,
-                    nutzername VARCHAR UNIQUE NOT NULL,
+                CREATE TABLE IF NOT EXISTS user_data (
+                    user_id TEXT PRIMARY KEY,
+                    username VARCHAR UNIQUE NOT NULL,
                     mail TEXT VARCHAR NOT NULL,
                     passwort_hash TEXT NOT NULL
                 )"""
@@ -359,10 +362,10 @@ class Database:
         db.create_table(conn_all_users, all_users_table)
 
         # Registriere einen neuen Benutzer
-        # db.l_register_user(conn, "testuser13", "test5@example.com", "password13")
+        # db.register_user(conn, "testuser13", "test5@example.com", "password13")
 
         # Logge den Benutzer ein
-        # db.l_login_user(conn, "testuser13", "password13")
+        # db.login_user(conn, "testuser13", "password13")
 
         # Gib die UserID des eingeloggten Benutzers aus
         print("User ID:", db.get_id())
@@ -389,7 +392,7 @@ class Database:
 
 
 
-    def p_insert_user(self, conn, tupel):
+    def insert_user(self, conn, tupel):
         # Values werden in die User Tabelle eingefügt
         try:
             # Values(?,?) sind Platzhalter die vom Tupel ersetzt werden
@@ -398,13 +401,17 @@ class Database:
             cur = conn.cursor()
             cur.execute(sql, tupel)
             conn.commit()
+
+            # Rückgabe der ID der zuletzt eingefügten Zeile, nützlich für 
+            # Referenzzwecke in nachfolgenden Operationen
             return cur.lastrowid
+        
         except sqlite3.Error as e:
             print(e)
 
 
 
-    def p_insert_project(self, conn, tupel):
+    def insert_project(self, conn, tupel):
         # Values werden in die Projekt Tabelle eingefügt
         try:
             sql = f''' INSERT INTO PROJECT(PID, NAME, DESCRIPTION, ADMIN, FUNDER)
@@ -418,7 +425,7 @@ class Database:
 
 
         
-    def p_insert_other_user(self, conn, nid, tupel):
+    def add_values_to_member(self, conn, nid, tupel):
         # Values werden in die Tabelle eines anderen Users eingefügt
         try:
             sql = f''' INSERT INTO {nid}(PID, ROLE)
